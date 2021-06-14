695fa820896cdb67e814586504efb0043b09cd5b
ChandrakantPal <sanjay161097@gmail.com>
2021-06-14 23:18:14 +0530

    brand model

---
+++ models/Brand.js
@@ -0,0 +1,21 @@
+const { model, Schema } = require('mongoose')
+
+const brandSchema = new Schema({
+  email: String,
+  brandname: String,
+  brandsymbol: String,
+  password: String,
+  createdAt: String,
+  followers: [
+    {
+      customerId: {
+        type: Schema.Types.ObjectId,
+        ref: 'customers',
+      },
+      loyaltyPoint: Number,
+    },
+  ],
+  loyaltyPoint: Number,
+})
+
+module.exports = model('Brand', brandSchema)
