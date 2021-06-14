4238bc7201e654d74b62e039091350f54380f177
ChandrakantPal <sanjay161097@gmail.com>
2021-06-14 23:18:48 +0530

    input validation

--- utils/validators.js
+++ utils/validators.js
@@ -2,15 +2,14 @@ module.exports.validateRegisterInput = (
   firstname,
   lastname,
   email,
-  password,
-  confirmPassword
+  password
 ) => {
   const errors = {}
   if (firstname.trim() === '') {
     errors.firstname = 'firstname must not be empty'
   }
   if (lastname.trim() === '') {
-    errors.lasttname = 'lastame must not be empty'
+    errors.lastname = 'lastname must not be empty'
   }
   if (email.trim() === '') {
     errors.email = 'Email must be empty'
@@ -23,8 +22,6 @@ module.exports.validateRegisterInput = (
   }
   if (password === '') {
     errors.password = 'Password must not be empty'
-  } else if (password !== confirmPassword) {
-    errors.confirmPassword = 'Passwords must match'
   }
 
   return {
@@ -41,6 +38,42 @@ module.exports.validateLoginInput = (email, password) => {
   if (password === '') {
     errors.password = 'Password must not be empty'
   }
+  return {
+    errors,
+    valid: Object.keys(errors).length < 1,
+  }
+}
+
+module.exports.validateBrandRegisterInput = (
+  brandname,
+  brandsymbol,
+  loyaltyPoint,
+  password,
+  email
+) => {
+  const errors = {}
+  if (brandname.trim() === '') {
+    errors.brandname = 'brandname must not be empty'
+  }
+  if (brandsymbol.trim() === '') {
+    errors.brandsymbol = 'brandsymbol must not be empty'
+  }
+  if (email.trim() === '') {
+    errors.email = 'Email must be empty'
+  } else {
+    const regEx =
+      /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/
+    if (!email.match(regEx)) {
+      errors.email = 'Email must be a valid email address'
+    }
+  }
+  if (password === '') {
+    errors.password = 'Password must not be empty'
+  }
+  if (loyaltyPoint <= 0) {
+    errors.loyaltyPoint = 'loyaltyPoint must be greater than zero'
+  }
+
   return {
     errors,
     valid: Object.keys(errors).length < 1,
