a1207c190b6766078549654d15cb27cdf97736f8
ChandrakantPal <sanjay161097@gmail.com>
2021-06-14 23:18:34 +0530

    user model

--- models/User.js
+++ models/User.js
@@ -1,11 +1,7 @@
 const { model, Schema } = require('mongoose')
 
 const userSchema = new Schema({
-  firstname: String,
-  lastname: String,
-  password: String,
   email: String,
-  createdAt: String,
   userType: String,
 })
 
