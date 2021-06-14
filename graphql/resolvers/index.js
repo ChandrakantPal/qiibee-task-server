3282035434f42e0cf9937ffd309f05b9f0fe5ba0
ChandrakantPal <sanjay161097@gmail.com>
2021-06-14 23:27:40 +0530

    userResolver.query added

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -4,6 +4,7 @@ const usersResolvers = require('./users')
 module.exports = {
   Query: {
     ...brandAndCustomerResolvers.Query,
+    ...usersResolvers.Query,
   },
   Mutation: {
     ...usersResolvers.Mutation,
