4bd7f3ede8910f25e232225d017b82326408057f
ChandrakantPal <sanjay161097@gmail.com>
2021-06-18 23:34:53 +0530

    get following followers redeem points resolvers

--- graphql/resolvers/brandAndCustomer.js
+++ graphql/resolvers/brandAndCustomer.js
@@ -14,18 +14,55 @@ module.exports = {
         throw new Error(error)
       }
     },
-    // async getCustomers(_, { customerIds }) {
-    //   try {
-    //     const ids = []
-    //     customerIds.map((customer) => {
-    //       ids.push(customer.customerId)
-    //     })
-    //     const customers = await Customer.find({ _id: { $in: ids } })
-    //     return customers
-    //   } catch (error) {
-    //     throw new Error(error)
-    //   }
-    // },
+    async getCustomers(_, { customerIds }, context) {
+      checkAuth(context)
+      try {
+        const ids = []
+        customerIds.map((customer) => {
+          ids.push(customer.customerId)
+        })
+        const customers = await Customer.find({ _id: { $in: ids } })
+        return customers
+      } catch (error) {
+        throw new Error(error)
+      }
+    },
+    async getFollowers(_, { brandId }, context) {
+      checkAuth(context)
+      try {
+        const brand = await Brand.findById(brandId)
+        if (brand) {
+          const ids = []
+          brand.followers.map((customer) => {
+            ids.push(customer.customerId)
+          })
+          const customers = await Customer.find({ _id: { $in: ids } })
+          return customers
+        } else {
+          throw new Error('Brand not found')
+        }
+      } catch (error) {
+        throw new Error(error)
+      }
+    },
+    async getFollowing(_, { customerId }, context) {
+      checkAuth(context)
+      try {
+        const customer = await Customer.findById(customerId)
+        if (customer) {
+          const ids = []
+          customer.following.map((brand) => {
+            ids.push(brand.brandId)
+          })
+          const brands = await Brand.find({ _id: { $in: ids } })
+          return brands
+        } else {
+          throw new Error('Customer not found')
+        }
+      } catch (error) {
+        throw new Error(error)
+      }
+    },
   },
   Mutation: {
     async followBrand(_, { brandId, customerId }, context) {
@@ -37,10 +74,12 @@ module.exports = {
           customer.following.unshift({
             brandId,
             loyaltyPoint: 0,
+            redeemed: false,
           })
           brand.followers.unshift({
             customerId,
             loyaltyPoint: 0,
+            redeemed: false,
           })
         }
         await customer.save()
@@ -77,6 +116,38 @@ module.exports = {
         await brand.save()
         await customer.save()
         return brand
+      } catch (error) {
+        throw new Error(error)
+      }
+    },
+    async redeemPoint(_, { customerId, brandId }, context) {
+      checkAuth(context)
+      try {
+        const customer = await Customer.findById(customerId)
+        const brand = await Brand.findById(brandId)
+
+        const followerIndex = brand.followers.findIndex(
+          (follower) => follower.customerId == customerId
+        )
+
+        if (followerIndex !== -1 && !brand.followers[followerIndex].redeemed) {
+          brand.followers[followerIndex].redeemed = true
+        }
+
+        const followingIndex = customer.following.findIndex(
+          (follow) => follow.brandId == brandId
+        )
+        if (
+          followingIndex !== -1 &&
+          !customer.following[followingIndex].redeemed
+        ) {
+          customer.following[followingIndex].redeemed = true
+          customer.totalloyaltyPoint +=
+            customer.following[followingIndex].loyaltyPoint
+        }
+        await brand.save()
+        await customer.save()
+        return customer
       } catch (error) {
         throw new Error(error)
       }
